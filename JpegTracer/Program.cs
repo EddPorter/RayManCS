c602ca5895affc59e7940fd2309bdd1f927c2133
EddPorter <github@eddporter.com>
2013-06-18 17:27:44 +0100

    Update scene example

--- JpegTracer/Program.cs
+++ JpegTracer/Program.cs
@@ -2,34 +2,53 @@
 
 namespace JpegTracer {
 
-internal static class Program {
+  internal static class Program {
 
-  public static void Main(string[] args) {
-    var output = new JpegOutput(640, 480);
-    var scene = new Scene(output);
-    scene.Camera = new OrthographicCamera(new Point(320.0f, 240.0f, -1000.0f), new Vector(0.0f, 1.0f, 0.0f), new Vector(1.0f, 0.0f, 0.0f), 640, 480);
+    public static void Main(string[] args) {
+      var output = new JpegOutput(640, 480);
+      var scene = new Scene(output);
+      scene.Camera = new OrthographicCamera(new Point(320.0f, 240.0f, -1000.0f), new Vector(0.0f, 1.0f, 0.0f), new Vector(1.0f, 0.0f, 0.0f), 640, 480);
 
-    scene.AddObject(new Sphere(new Point(233.0f, 290.0f, 0.0f), 100.0f));
-    scene.AddObject(new Sphere(new Point(407.0f, 290.0f, 0.0f), 100.0f) {
-      Material = new Material {
-        Colour = System.Drawing.Color.Cyan
-      }
-    });
-    scene.AddObject(new Sphere(new Point(320.0f, 140.0f, 0.0f), 100.0f) {
-      Material = new Material {
-        Colour = System.Drawing.Color.Magenta
-      }
-    });
+      scene.AddObject(new Sphere(new Point(350.0f, 290.0f, 320.0f), 50.0f) {
+        Material = new Material {
+          Colour = System.Drawing.Color.FromArgb((int)(0.35f * 255), (int)(0.25f * 255), (int)(0.01f * 255))
+        }
+      });
+      scene.AddObject(new Sphere(new Point(600.0f, 290.0f, 480.0f), 100.0f) {
+        Material = new Material {
+          Colour = System.Drawing.Color.White
+        }
+      });
+      scene.AddObject(new Sphere(new Point(450.0f, 140.0f, 400.0f), 50.0f) {
+        Material = new Material {
+          Colour = System.Drawing.Color.FromArgb((int)(0.5f * 255), (int)(0.5f * 255), (int)(0.5f * 255))
+        }
+      });
 
-    scene.AddLight(new DiffuseLight(new Point(0.0f, 240.0f, -100.0f)) {
-      Colour = System.Drawing.Color.White
-    });
-    scene.AddLight(new DiffuseLight(new Point(640.0f, 240.0f, -10000.0f)) {
-      Colour = System.Drawing.Color.FromArgb((int)(0.6f * 255), (int)(0.7f * 255), 255)
-    });
+      var blobColour = System.Drawing.Color.White;
+      var blobMaterial = new Material {
+        Colour = blobColour
+      };
+      scene.AddObject(new Sphere(new Point(60.0f, 290.0f, 320.0f), 80.0f) {
+        Material = blobMaterial
+      });
+      scene.AddObject(new Sphere(new Point(400.0f, 290.0f, 480.0f), 80.0f) {
+        Material = blobMaterial
+      });
+      scene.AddObject(new Sphere(new Point(250.0f, 140.0f, 400.0f), 80.0f) {
+        Material = blobMaterial
+      });
 
-    scene.Draw();
-    output.Save("output.jpg");
+
+      scene.AddLight(new DiffuseLight(new Point(0.0f, 240.0f, 300.0f)) {
+        Colour = System.Drawing.Color.White
+      });
+      scene.AddLight(new DiffuseLight(new Point(640.0f, 480.0f, -100.0f)) {
+        Colour = System.Drawing.Color.FromArgb((int)(0.6f * 255), (int)(0.7f * 255), (int)(1.0f * 255))
+      });
+
+      scene.Draw();
+      output.Save("output.jpg");
+    }
   }
-}
 }
--- WindowTracer/RealtimeWindow.cs
+++ WindowTracer/RealtimeWindow.cs
@@ -15,15 +15,45 @@ internal sealed partial class RealtimeWindow : Form {
     InitializeComponent();
     output = new RealtimeWindowOutput(640, 480, this);
     scene = new Scene(output);
-    scene.Camera = new OrthographicCamera(new RayManCS.Point(0.0f, 0.0f, 0.0f), new Vector(0.0f, 1.0f, 0.0f), new Vector(1.0f, 0.0f, 0.0f), 80, 60);
+    scene.Camera = new OrthographicCamera(new Point(320.0f, 240.0f, -1000.0f), new Vector(0.0f, 1.0f, 0.0f), new Vector(1.0f, 0.0f, 0.0f), 640, 480);
 
-    scene.AddObject(new Sphere(new Point(-5.0f, -5.0f, 50.0f), 10.0f));
-    scene.AddObject(new Sphere(new Point(-20.0f, 10.0f, 150.0f), 5.0f));
-    scene.AddObject(new Sphere(new Point(20.0f, 10.0f, 100.0f), 15.0f));
-    scene.AddObject(new Sphere(new Point(0.0f, -15.0f, 150.0f), 5.0f));
+    scene.AddObject(new Sphere(new Point(400.0f, 290.0f, 320.0f), 50.0f) {
+      Material = new Material {
+        Colour = System.Drawing.Color.FromArgb((int)(0.35f * 255), (int)(0.25f * 255), (int)(0.01f * 255))
+      }
+    });
+    scene.AddObject(new Sphere(new Point(600.0f, 290.0f, 480.0f), 100.0f) {
+      Material = new Material {
+        Colour = System.Drawing.Color.White
+      }
+    });
+    scene.AddObject(new Sphere(new Point(450.0f, 140.0f, 400.0f), 50.0f) {
+      Material = new Material {
+        Colour = System.Drawing.Color.FromArgb((int)(0.5f * 255), (int)(0.5f * 255), (int)(0.5f * 255))
+      }
+    });
 
-    scene.AddLight(new DiffuseLight(new Point(-50.0f, 0.0f, 0.0f)));
-    scene.AddLight(new DiffuseLight(new Point(0.0f, 1000.0f, 1000.0f)));
+    var blobColour = System.Drawing.Color.White;
+    var blobMaterial = new Material {
+      Colour = blobColour
+    };
+    scene.AddObject(new Sphere(new Point(60.0f, 290.0f, 320.0f), 80.0f) {
+      Material = blobMaterial
+    });
+    scene.AddObject(new Sphere(new Point(400.0f, 290.0f, 480.0f), 80.0f) {
+      Material = blobMaterial
+    });
+    scene.AddObject(new Sphere(new Point(250.0f, 140.0f, 400.0f), 80.0f) {
+      Material = blobMaterial
+    });
+
+
+    scene.AddLight(new DiffuseLight(new Point(0.0f, 240.0f, 300.0f)) {
+      Colour = System.Drawing.Color.White
+    });
+    scene.AddLight(new DiffuseLight(new Point(640.0f, 480.0f, -100.0f)) {
+      Colour = System.Drawing.Color.FromArgb((int)(0.6f * 255), (int)(0.7f * 255), (int)(1.0f * 255))
+    });
   }
 
   private void Form1_FormClosing(object sender, FormClosingEventArgs e) {
